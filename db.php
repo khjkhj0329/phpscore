9f018cf0ac033d24e79226cef27211ddac3afd82
KimHyoJung <w2029@e-mirim.hs.kr>
2021-12-09 16:06:26 +0900

    DB 초기 파일

--- db.php
+++ db.php
@@ -7,21 +7,22 @@ $db['db'] = 'mirim';  // db 이름
 
 $mysqli = new mysqli($db['host'], $db['user'], $db['pw'], $db['db']);
 if ($mysqli->connect_error) {
-  die("서버 연결 실패: " . $conn->connect_error);
+  die("서버 연결 실패: " . $conn->connect_error); 
 }
 
-/*
-// DB 동작 테스트 코드 - 존재하는 데이터베이스 목록 출력
-$result = $mysqli->query("SHOW DATABASES;");
-echo ("Databases...<br><hr>");
 
-foreach ($result as $row){
-  // echo('<hr><pre>');
-    //    print_r($row);
-    // echo('</pre>');
-    echo $row['Database'] . "<br>";
-}
-*/
+// // DB 동작 테스트 코드 - 존재하는 데이터베이스 목록 출력
+// $result = $mysqli->query("SHOW DATABASES;");
+// echo ("Databases...<br><hr>");
+
+// foreach ($result as $row){
+//   // echo('<hr><pre>');
+//     //    print_r($row);
+//     // echo('</pre>');
+//     echo $row['Database'] . "<br>";
+// }
+
+
 
 
 /*
